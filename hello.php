474df69f4e9f6e73acf3cb94d4604ebc7c84b4cb
marko_jez <marko_jez@live.com>
2025-03-29 21:40:29 +0000

    Dodavanje html koda

--- hello.php
+++ hello.php
@@ -1,3 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form>
+        <input type="text" placeholder="Enter text">
+        <input type="submit">
+    </form>
 
-echo "Bok";
+</body>
+</html>
